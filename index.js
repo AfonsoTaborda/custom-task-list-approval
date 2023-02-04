3888c1ae5d568abe5184e5135fc1a42c21a12823
AfonsoTaborda <afonso.taborda@netcabo.pt>
2023-02-04 22:09:21 +0100

    Created Dynamic user comment generator

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+    try {
+        // This should be a token with access to your repository scoped in as a secret.
+        // The YML workflow will need to set myToken with the GitHub Secret Token
+        // myToken: ${{ secrets.GITHUB_TOKEN }}
+        // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+        const myToken = core.getInput('token');
+        const userCommentBody = core.getInput('body');
+        if(! typeof userCommentBody === 'string') [
+            github.context.action.setFailed("The body input is not of type 'string'!")
+        ]
+
+        const octokit = github.getOctokit(myToken);
+
+        const { data: pullRequestComments } = await octokit.rest.pulls.listReviewComments({
+            owner: github.context.owner,
+            repo: github.context.repo,
+            pull_number: github.context.pull_number,
+        });
+
+        var similarCommentsCount = 0;
+
+        if(pullRequestComments.length != 0) {
+            for (let comment of pullRequestComments) {
+                if(comment.body.includes(userCommentBody)) {
+                    similarCommentsCount++;
+                }
+            }
+        }
+
+        if(similarCommentsCount === 0) {
+            await octokit.rest.issues.createComment({
+                owner: github.context.owner,
+                repo: github.context.repo,
+                issue_number: github.context.issue_number,
+                body: userCommentBody,
+            });
+        }
+      } catch (error) {
+        core.setFailed(error.message);
+      }
 }
+
+run();
