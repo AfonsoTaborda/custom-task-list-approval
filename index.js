707911b1686592576daf404e2c60746d02d01455
AfonsoTaborda <afonso.taborda@netcabo.pt>
2023-02-04 22:57:53 +0100

    .

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ async function run() {
         // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
         const myToken = core.getInput('token');
         const userCommentBody = core.getInput('body');
-        if(!typeof userCommentBody === 'string') [
+        if(!typeof userCommentBody === 'string') {
             core.setFailed("The body input is not of type 'string'!")
-        ]
+        }
 
         const octokit = github.getOctokit(myToken);
 
