2a16af87fc8cc39fc2ebaf9ec7c732010ad27748
AfonsoTaborda <afonso.taborda@netcabo.pt>
2023-02-05 15:22:36 +0100

    Refactoring

--- github-comment.js
+++ github-comment.js
@@ -1,6 +1,48 @@
 const github = require('@actions/github');
 const pause = require('./pauser');
 
+const inputs = {
+    myToken: core.getInput('token'),
+    userChecklist: core.getInput('checklist-items'),
+    title: core.getInput('comment-title'),
+    body: core.getInput('comment-body'),
+    timeout: parseInt(core.getInput('completion-timeout')),
+}
+
+async function initializeComment() {
+    if (!typeof inputs.userChecklist === 'string') {
+        core.setFailed("The body input is not of type 'string'!");
+    }
+
+    const octokit = github.getOctokit(inputs.myToken);
+        
+    var resultComment = "";
+
+    if (inputs.title) {
+        resultComment += "# " + inputs.title + "\n";
+    }
+
+    if (inputs.body) {
+        resultComment += inputs.body + "\n";
+    }
+
+    const { data: pullRequestComments } = await octokit.rest.issues.listComments({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: github.context.issue.number,
+    });
+
+    // Loop through the user added checklist items,
+    // And append them into the resulting comment
+    for (let item of inputs.userChecklist.split(";")) {
+        if(item) {
+            resultComment += "- [ ] " + item + "\n";
+        }
+    }
+
+    return octokit, inputs.timeout, pullRequestComments, resultComment, inputs.userChecklist, inputs.title, inputs.body;
+}
+
 async function createGithubComment(octokit, commentBody) {
     console.log("No similar comments found, creating the comment...");
     var { data: comment } = await octokit.rest.issues.createComment({
@@ -54,6 +96,7 @@ async function deleteGithubComment(octokit, commentId) {
 }
 
 module.exports = {
+    initializeComment,
     getGithubComment,
     createGithubComment,
     deleteGithubComment,
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const runTimer = require('./timer');
-const {createGithubComment} = require('./github-comment');
+const {createGithubComment, initializeComment} = require('./github-comment');
 
 const CHECK_LIST_REGEX = /\[(x|X|\s)\](.*)/g;
 
@@ -11,33 +10,7 @@ async function run() {
         // The YML workflow will need to set myToken with the GitHub Secret Token
         // myToken: ${{ secrets.GITHUB_TOKEN }}
         // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-        const myToken = core.getInput('token');
-        const userChecklist = core.getInput('checklist-items');
-        const title = core.getInput('comment-title');
-        const body = core.getInput('comment-body');
-        const timeout = parseInt(core.getInput('completion-timeout'));
-
-        if (!typeof userChecklist === 'string') {
-            core.setFailed("The body input is not of type 'string'!");
-        }
-
-        const octokit = github.getOctokit(myToken);
-        
-        var resultComment = "";
-
-        if (title) {
-            resultComment += "# " + title + "\n";
-        }
-
-        if (body) {
-            resultComment += body + "\n";
-        }
-
-        const { data: pullRequestComments } = await octokit.rest.issues.listComments({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            issue_number: github.context.issue.number,
-        });
+        var octokit, timeout, pullRequestComments, resultComment, userChecklist, title, body = await initializeComment();
 
         // Check if there are similar comments already posted
         var similarCommentId;
@@ -47,14 +20,6 @@ async function run() {
                     similarCommentId = comment.id;
                     console.log(`A similar comment has been found with id: ${similarCommentId}`);
                 }
-            }
-        }
-
-        // Loop through the user added checklist items,
-        // And append them into the resulting comment
-        for (let item of userChecklist.split(";")) {
-            if(item) {
-                resultComment += "- [ ] " + item + "\n";
             }
         }
 
