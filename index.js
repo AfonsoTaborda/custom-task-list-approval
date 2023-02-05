5f07fb2861a411ac3026a74956b5e8b3403d5aff
AfonsoTaborda <afonso.taborda@netcabo.pt>
2023-02-05 09:59:21 +0100

    Made a separate functionf or the timer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const timer = require('./timer');
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -65,58 +66,7 @@ async function run() {
             throw "The comment to be added is empty!";
         }
 
-        var timer = setInterval(async function(){
-            console.log("Starting the timer...");
-            var sec = timeout * 60;
-
-            // If there are no similar comments, then post the comment
-            if (similarCommentsCount === 0) {
-                console.log("No similar comments found, creating the comment...");
-                var { data: comment } = await octokit.rest.issues.createComment({
-                    owner: github.context.repo.owner,
-                    repo: github.context.repo.repo,
-                    issue_number: github.context.issue.number,
-                    body: resultComment,
-                });
-
-                console.log(`Created a new checklist comment ${comment.body}`);
-            } else {
-                var { data: comment } = await octokit.rest.issues.getComment({
-                    owner: github.context.repo.owner,
-                    repo: github.context.repo.repo,
-                    comment_id: similarCommentId,
-                });
-
-                console.log(`Fetched the existing comment ${comment.body}`);
-            }
-
-            if (!comment) {
-                throw "The source comment could not be fetched";
-            }
-
-            var isCompleteArr = [];
-            let count = 0;
-            while ((match = TASK_LIST_ITEM.exec(comment.body)) !== null) {
-                var isComplete = match[1] != " ";
-                var itemText = match[2];
-
-                count++;
-
-                if (isComplete && !isCompleteArr.includes(itemText)) {
-                    isCompleteArr.push(itemText);
-                    console.log(`${itemText} is complete ✅`);
-                } else {
-                    console.log(`${itemText} has not been completed yet ❌`);
-                }
-            }
-
-            sec--;
-
-            if (sec < 0 || isCompleteArr.length == count && count != 0) {
-                console.log(`Clearing the timeout with sec = ${sec} and isCompleteArr.length = ${isCompleteArr.length}`);
-                clearInterval(timer);
-            }
-        }, 1000);
+        timer.timer(timeout, similarCommentsCount, similarCommentId, resultComment, octokit, TASK_LIST_ITEM);
       } catch (error) {
         core.setFailed(error);
       }
