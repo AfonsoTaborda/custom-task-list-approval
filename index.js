a328f8e984028076aace007e0ff930d4d8b2930a
AfonsoTaborda <afonso.taborda@netcabo.pt>
2023-02-05 09:15:51 +0100

    .

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ async function run() {
 
             sec--;
 
-            if (sec < 0 || isCompleteArr.length == count && count != 0) {
+            if (sec < 0 || isCompleteArr.length == count) {
                 console.log(`Clearing the timeout with sec = ${sec} and isCompleteArr.length = ${isCompleteArr.length}`);
                 clearInterval(timer);
             }
