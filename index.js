9bc2f46adc9045e0d5396883e040943431f8adb6
AfonsoTaborda <afonso.taborda@netcabo.pt>
2023-02-05 05:37:09 +0100

    .

--- index.js
+++ index.js
@@ -8,12 +8,25 @@ async function run() {
         // myToken: ${{ secrets.GITHUB_TOKEN }}
         // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
         const myToken = core.getInput('token');
-        const userCommentBody = core.getInput('body');
-        if(!typeof userCommentBody === 'string') {
-            core.setFailed("The body input is not of type 'string'!")
+        const userChecklist = core.getInput('checklist-items');
+        const title = core.getInput('comment-title');
+        const header = core.getInput('comment-header');
+        const body = core.getInput('comment-body');
+
+        if(!typeof userChecklist === 'string') {
+            core.setFailed("The body input is not of type 'string'!");
         }
 
         const octokit = github.getOctokit(myToken);
+        var resultComment = "";
+
+        if (title) {
+            resultComment += "#" + title + "\n";
+        }
+
+        if (body) {
+            resultComment += body + "\n";
+        }
 
         const { data: pullRequestComments } = await octokit.rest.issues.listComments({
             owner: github.context.repo.owner,
@@ -22,21 +35,31 @@ async function run() {
         });
 
         var similarCommentsCount = 0;
-
         if(pullRequestComments.length != 0) {
             for (let comment of pullRequestComments) {
-                if(comment.body.includes(userCommentBody)) {
+                if(comment.body.includes(userChecklist)) {
                     similarCommentsCount++;
                 }
             }
         }
 
+        // Loop through the user added checklist items,
+        // And append them into the resulting comment
+        for (let item of userChecklist.split(";")) {
+            resultComment += "- [ ] " + item + "\n";
+        }
+
+        if(resultComment === "") {
+            throw "The comment to be added is empty!";
+        }
+
+        // If there are no similar comments, then post the comment
         if(similarCommentsCount === 0) {
             await octokit.rest.issues.createComment({
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
                 issue_number: github.context.issue.number,
-                body: userCommentBody,
+                body: resultComment,
             });
         }
       } catch (error) {
